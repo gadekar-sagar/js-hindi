b173d91f97ef3b3bd3776df8fe72a428da0a4c8c
gadekar-sagar <148203916+gadekar-sagar@users.noreply.github.com>
2024-08-30 15:13:42 +0000

    Data Conversion

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,30 @@
-console.log('sagar Gadekar');
+// age =25;
+// console.log(typeof age);
+// console.log(typeof null);
+
+// let score=33;
+// console.log(typeof score)
+
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score=undefined;
+// console.log('chech data type score=:'+typeof score)
+
+// let valueInNumber=Number(score);
+// console.log('check data type valueInNumber=:'+typeof valueInNumber);
+// console.log('check data type valueInNumber=:'+valueInNumber);
+
+// let isLoggedIn=1;
+// console.log('check data Type isLoggedIn=:'+isLoggedIn);
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(' booleanIsLoggedIn=:'+booleanIsLoggedIn);
+// console.log('check data Type booleanIsLoggedIn=:'+ typeof booleanIsLoggedIn);
+
+let isLoggedIn="";
+console.log('check data Type isLoggedIn=:'+isLoggedIn);
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(' booleanIsLoggedIn=:'+booleanIsLoggedIn);
+console.log('check data Type booleanIsLoggedIn=:'+ typeof booleanIsLoggedIn);
+
